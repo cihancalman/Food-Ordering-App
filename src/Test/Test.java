fd4bbf131a1437d48495ec3fcce5859679892c53
cihancalman <cihancalman1@gmail.com>
2023-12-23 20:28:49 +0300

    add Test

---
+++ src/Test/Test.java
@@ -0,0 +1,13 @@
+package Test;
+
+import Views.LoginGUI;
+
+
+
+public class Test {
+
+    public static void main(String[] args) {
+        LoginGUI login = new LoginGUI();
+        login.setVisible(true);
+    }
+}
